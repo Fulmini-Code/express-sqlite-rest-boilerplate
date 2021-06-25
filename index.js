85cccd5a8b7eec02a0147f8f4eb87255f3b05136
Andrei Dogaru <andreidogaru99@gmail.com>
2021-06-25 15:34:57 +0300

    Version 1.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv')
 const express = require('express')
+const cors = require('cors')
 
 const db = require('./database')
 
@@ -11,6 +12,8 @@ const port = process.env.PORT
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server started @ http://localhost:${port}/`)
